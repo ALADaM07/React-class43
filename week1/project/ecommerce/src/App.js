d042abc91717c2fc6c78f8857ac08b328e487b6d
Mohamad Alamin <alamin.mohamad.a0@gmail.com>
2023-07-12 00:18:05 +0200

    highlighted the catagory that the user selected so the user can see which one is it

--- week1/project/ecommerce/src/App.js
+++ week1/project/ecommerce/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
@@ -20,32 +21,47 @@ const App = () => {
   }, []);
 
   const filterProducts = (category) => {
-    if (category === 'all') {
+    if (category === 'All') {
       setFilteredProducts(products);
+      setSelectedCategory('All');
     } else {
       const filtered = products.filter(
         (product) => product.category === category
       );
       setFilteredProducts(filtered);
+      setSelectedCategory(category);
     }
   };
 
   return (
     <div className='App'>
-      <h1>Fake Products</h1>
+      <h1>Web Shop</h1>
       <div className='category-buttons'>
-        <button onClick={() => filterProducts('all')}>All</button>
+        <button
+          onClick={() => filterProducts('All')}
+          className={selectedCategory === 'All' ? 'active' : ''}>
+          All
+        </button>
         {categories.map((category) => (
-          <button key={category} onClick={() => filterProducts(category)}>
+          <button
+            key={category}
+            onClick={() => filterProducts(category)}
+            className={selectedCategory === category ? 'active' : ''}>
             {category}
           </button>
         ))}
+      </div>
+      <div className='selected-category'>
+        Selected Category: {selectedCategory}
       </div>
       <div className='product-list'>
         {filteredProducts.map((product) => (
           <div key={product.id} className='product-card'>
             <img src={product.image} alt={product.title} />
             <h3>{product.title}</h3>
+            <p>{product.price}</p>
+            <p>{product.category}</p>
+            <button>Add to Cart</button>
           </div>
         ))}
       </div>
